0c4bfbb6669d9966539734ed29b029e1bf144a75
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-07-28 22:55:55 +0300

    Add `MappedSignal`

--- js/app.ts
+++ js/app.ts
@@ -68,6 +68,61 @@ class SourceSignal<T> implements Signal<T>, Reset<T> {
     }
 }
 
+class MappedSignal<T, U> implements Signal<U> {
+    private readonly subscribers = new Set<Subscriber<U>>();
+    private v: U | undefined = undefined;
+    private readonly depSubscriber: Subscriber<T>;
+    
+    constructor(
+        private readonly equals: (x: U, y: U) => boolean,
+        private readonly f: (depVal: T) => U,
+        private readonly dependency: Signal<T>
+    ) {
+        this.depSubscriber = (_: T, newDepVal: T) => {
+            const oldVal = this.v as U; // TODO: Ensure safety
+            const newVal = this.f(newDepVal);
+            this.v = newVal;
+            this.notify(oldVal, newVal);
+        };
+    }
+    
+    ref(): U {
+        if (this.subscribers.size === 0) {
+            this.v = this.f(this.dependency.ref()); // OPTIMIZE: Could this call could be cached?
+        }
+        
+        return this.v as U; // TODO: Ensure safety
+    }
+    
+    subscribe(subscriber: Subscriber<U>) {
+        if (this.subscribers.size === 0) {
+            // To avoid space leaks and 'unused' updates to `this` only start watching dependencies
+            // when `this` gets its first watcher:
+            this.dependency.subscribe(this.depSubscriber);
+        }
+        
+        this.subscribers.add(subscriber);
+    }
+    
+    unsubscribe(subscriber: Subscriber<U>) {
+        this.subscribers.delete(subscriber);
+        
+        if (this.subscribers.size === 0) {
+            // Watcher count just became zero, but watchees still have pointers to `this` (via
+            // `depSubscriber`). Remove those to avoid space leaks and 'unused' updates to `this`:
+            this.dependency.unsubscribe(this.depSubscriber);
+        }
+    }
+    
+    notify(v: U, u: U) {
+        if (!this.equals(v, u)) {
+            for (const subscriber of this.subscribers) {
+                subscriber(v, u);
+            }
+        }
+    }
+}
+
 (function (window) {
 	'use strict';
 
