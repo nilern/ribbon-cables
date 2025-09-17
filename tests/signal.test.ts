61c975a82bba08391ba40169906ec4db6c65dce4
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-09-17 13:59:12 +0300

    Test `source`

--- tests/signal.test.ts
+++ tests/signal.test.ts
@@ -1,9 +1,39 @@
-import {stable} from "../js/signal";
+import {stable, source} from "../js/signal";
+
+import {eq} from "../js/prelude";
 
 describe('testing `stable`', () => {
-  test('ref()', () => {
-    const answerS = stable(42);
-    expect(answerS.ref()).toBe(42);
-  });
+    test('ref()', () => {
+        const answerS = stable(42);
+        
+        expect(answerS.ref()).toBe(42);
+    });
+});
+
+describe('testing `source`', () => {
+    test('ref()', () => {
+        const answerS = source(eq, 42);
+        
+        expect(answerS.ref()).toBe(42);
+    });
+    
+    test('reset()', () => {
+        const answerS = source(eq, 42);
+        let changedV = undefined;
+        answerS.addSubscriber({onChange: (v) => changedV = v});
+        
+        answerS.reset(5);
+        
+        expect(answerS.ref()).toBe(5);
+        expect(changedV).toBe(5);
+        
+        let notified = false;
+        answerS.addSubscriber({onChange: (_) => notified = true});
+        
+        answerS.reset(5);
+        
+        expect(answerS.ref()).toBe(5);
+        expect(notified).toBeFalsy();
+    });
 });
 
