6f3687bac1772d1431131ff8c82b131e09cb2831
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-10-15 11:47:15 +0300

    Prop test `imux`

--- tests/vecnal.props.test.ts
+++ tests/vecnal.props.test.ts
@@ -3,38 +3,10 @@ import {test as tst, fc} from '@fast-check/jest';
 
 import * as vec from '../js/vecnal.js';
 
+import * as sig from '../js/signal.js';
+import {eq, ImmArrayAdapter} from '../js/prelude.js';
+
 const maxLength = 100;
-
-class User {
-    constructor(
-        public readonly id: number,
-        public username: string
-    ) {}
-    
-    eq(that: User): boolean { return this.id === that.id; }
-}
-
-function compareUsernames(user1: User, user2: User): number {
-    return user1.username.localeCompare(user2.username);
-}
-
-tst.prop({usernames: fc.array(fc.string())})(
-    'output is input sorted',
-    ({usernames}) => {
-        const initialUsers = usernames.map((username, id) => new User(id, username));
-        
-        const userS = vec.source((user1, user2) => user1.eq(user2), initialUsers);
-        const sortedUserS = userS.sort(compareUsernames);
-        
-        const vecnalSorted = sortedUserS.reduce((acc, user) => {
-            acc.push(user);
-            return acc;
-        }, []);
-        const arraySorted = [...initialUsers].sort(compareUsernames);
-        
-        expect(vecnalSorted).toEqual(arraySorted);
-    }
-);
 
 type Insertion = {
     name: 'insert',
@@ -72,8 +44,106 @@ const arbOp: Arb<Op> = fc.oneof(
     })
 );
 
+class User {
+    constructor(
+        public readonly id: number,
+        public username: string
+    ) {}
+    
+    eq(that: User): boolean { return this.id === that.id; }
+}
+
+function compareUsernames(user1: User, user2: User): number {
+    return user1.username.localeCompare(user2.username);
+}
+
+tst.prop({usernames: fc.array(fc.string())})(
+    '`imux` output is input elements',
+    ({usernames}) => {
+        const usernameS = sig.stable(new ImmArrayAdapter(usernames));
+        const usernameZ = vec.imux(eq, usernameS);
+        
+        const vecnalUsernames = usernameZ.reduce((acc, username) => {
+            acc.push(username);
+            return acc;
+        }, []);
+        
+        expect(vecnalUsernames).toEqual(usernames);
+    }
+);
+
 tst.prop({usernames: fc.array(fc.string(), {maxLength}), ops: fc.array(arbOp)})(
-    'output after input modifications is still input sorted',
+    '`imux` output after input modifications is still input elements',
+    ({usernames, ops}) => {
+        const usernameS = sig.source(eq, new ImmArrayAdapter([...usernames]));
+        const usernameZ = vec.imux(eq, usernameS);
+        usernameZ.addISubscriber({
+            onInsert: (_, _1) => {},
+            onRemove: (_) => {},
+            onSubstitute: (_, _1) => {}
+        });
+        
+        for (const op of ops) {
+            const currentUsernames = usernameS.ref();
+        
+            switch (op.name) {
+            case 'insert':
+                if (op.index <= currentUsernames.length) {
+                    const newUsernames = [...currentUsernames];
+                    newUsernames.splice(op.index, 0, op.username);
+                    usernameS.reset(new ImmArrayAdapter(newUsernames));
+                }
+                break;
+            
+            case 'remove':
+                if (op.index < currentUsernames.length) {
+                    const newUsernames = [...currentUsernames];
+                    newUsernames.splice(op.index, 1);
+                    usernameS.reset(new ImmArrayAdapter(newUsernames));
+                }
+                break;
+            
+            case 'substitute':
+                if (op.index < currentUsernames.length) {
+                    const newUsernames = [...currentUsernames];
+                    newUsernames[op.index] = op.username;
+                    usernameS.reset(new ImmArrayAdapter(newUsernames));
+                }
+                break;
+            
+            default: { const _exhaust: never = op.name; }
+            }
+        }
+        
+        const vecnalUsernames = usernameZ.reduce((acc, username) => {
+            acc.push(username);
+            return acc;
+        }, []);
+        
+        expect(vecnalUsernames).toEqual(usernames);
+    }
+);
+
+tst.prop({usernames: fc.array(fc.string())})(
+    '`sort` output is input sorted',
+    ({usernames}) => {
+        const initialUsers = usernames.map((username, id) => new User(id, username));
+        
+        const userS = vec.source((user1, user2) => user1.eq(user2), initialUsers);
+        const sortedUserS = userS.sort(compareUsernames);
+        
+        const vecnalSorted = sortedUserS.reduce((acc, user) => {
+            acc.push(user);
+            return acc;
+        }, []);
+        const arraySorted = [...initialUsers].sort(compareUsernames);
+        
+        expect(vecnalSorted).toEqual(arraySorted);
+    }
+);
+
+tst.prop({usernames: fc.array(fc.string(), {maxLength}), ops: fc.array(arbOp)})(
+    '`sort` output after input modifications is still input sorted',
     ({usernames, ops}) => {
         const initialUsers = usernames.map((username, id) => new User(id, username));
         let currentId = initialUsers.length;
