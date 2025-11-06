40be4e869393c3ce714a524167e3aa1b45ee31f3
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-11-06 12:11:37 +0200

    Add (no-op) buttons

--- examples/js-framework-benchmark/js/main.ts
+++ examples/js-framework-benchmark/js/main.ts
@@ -1,17 +1,40 @@
 "use strict";
 
 import * as dom from "../../../lib/dom.js";
-import type {NodeFactory} from "../../../lib/dom.js";
+import type {NodeFactory, EventHandler} from "../../../lib/dom.js";
 
 function heading(nodes: NodeFactory): Node {
     return nodes.el("row", {class: "col-md-6"},
         nodes.el("h1", {}, "RibbonCables"));
 }
+             
+function benchButton(nodes: NodeFactory, id: string, label: string, onClick: EventHandler): Node {
+    return nodes.el("div", {class: "col-sm-6 smallpad"},
+        nodes.el("button",  {
+            class: "btn btn-primary btn-block",
+            type: "button",
+            id: id,
+            onclick: onClick
+        }, label))
+}
+
+function benchButtons(nodes: NodeFactory): Node {
+    return nodes.el("div", {class: "col-md-6"},
+        nodes.el("div", {class: "row"},
+           benchButton(nodes, "run", "Create 1,000 rows", (_) => {}),
+           benchButton(nodes, "runlots", "Create 10,000 rows", (_) => {}),
+           benchButton(nodes, "add", "Append 1,000 rows", (_) => {}),
+           benchButton(nodes, "update", "Update every 10th row", (_) => {}),
+           benchButton(nodes, "clear", "Clear", (_) => {}),
+           benchButton(nodes, "swaprows", "Swap rows", (_) => {})));
+}
 
 function createUI(nodes: NodeFactory): Node {
     return nodes.el("div", {class: "container"},
         nodes.el("div", {class: "jumbotron"},
-            heading(nodes)));
+            nodes.el("div", {class: "row"},
+                heading(nodes),
+                benchButtons(nodes))));
 }
 
 (() => {
