9935681b23723e3d528b8456e1411a62bb61dcd0
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-11-07 11:12:33 +0200

    Add table

--- examples/js-framework-benchmark/js/main.ts
+++ examples/js-framework-benchmark/js/main.ts
@@ -1,7 +1,129 @@
 "use strict";
 
+import type {Reset} from "../../../lib/prelude.js";
+import {eq, ImmArrayAdapter} from "../../../lib/prelude.js"; // HACK
+import type {Framer} from "../../../lib/dom.js";
 import * as dom from "../../../lib/dom.js";
 import type {NodeFactory, EventHandler} from "../../../lib/dom.js";
+import {Signal} from "../../../lib/signal.js";
+import * as sig from "../../../lib/signal.js";
+import {Vecnal} from "../../../lib/vecnal.js";
+import * as vec from "../../../lib/vecnal.js";
+
+function randNat(max: number) { return Math.floor(Math.random() * max); }
+
+const adjectives: readonly string[] = [
+  "pretty",
+  "large",
+  "big",
+  "small",
+  "tall",
+  "short",
+  "long",
+  "handsome",
+  "plain",
+  "quaint",
+  "clean",
+  "elegant",
+  "easy",
+  "angry",
+  "crazy",
+  "helpful",
+  "mushy",
+  "odd",
+  "unsightly",
+  "adorable",
+  "important",
+  "inexpensive",
+  "cheap",
+  "expensive",
+  "fancy",
+];
+
+const colours: readonly string[] = [
+    "red",
+    "yellow",
+    "blue",
+    "green",
+    "pink",
+    "brown",
+    "purple",
+    "brown",
+    "white",
+    "black",
+    "orange"
+];
+
+const nouns: readonly string[] = [
+  "table",
+  "chair",
+  "house",
+  "bbq",
+  "desk",
+  "car",
+  "pony",
+  "cookie",
+  "sandwich",
+  "burger",
+  "pizza",
+  "mouse",
+  "keyboard",
+];
+
+class Datum {
+    constructor(
+        public readonly id: number,
+        public readonly label: string
+    ) {}
+}
+
+class Model {
+    constructor(
+        public readonly nextId: number = 0,
+        public readonly data: readonly Datum[] = []
+    ) {}
+    
+    rebuild(count: number): Model {
+        let nextId = this.nextId;
+        const data = new Array(count);
+        
+        for (let i = 0; i < count; ++i) {
+            data[i] = new Datum(
+                nextId++,
+                adjectives[randNat(adjectives.length)] + " " +
+                colours[randNat(colours.length)] + " " +
+                nouns[randNat(nouns.length)]
+            );
+        }
+        
+        return new Model(nextId, data);
+    }
+    
+    withoutRow(id: number): Model {
+        return new Model(
+            this.nextId,
+            this.data.filter((datum) => datum.id !== id)
+        );
+    }
+}
+
+class Ctrl {
+    constructor(
+        private readonly nodes: Framer,
+        private readonly modelS: Signal<Model> & Reset<Model>,
+        private readonly selectedS: Signal<number | undefined> & Reset<number | undefined>
+    ) {}
+    
+    rebuild(count: number) {
+        this.nodes.frame(() => this.modelS.reset(this.modelS.ref().rebuild(count)));
+    }
+    
+    selectRow(id: number) { this.nodes.frame(() => this.selectedS.reset(id)); }
+    
+    deleteRow(id: number) {
+        this.nodes.frame(() => this.modelS.reset(this.modelS.ref().withoutRow(id)));
+    }
+}
 
 function heading(nodes: NodeFactory): Node {
     return nodes.el("row", {class: "col-md-6"},
@@ -18,28 +140,102 @@ function benchButton(nodes: NodeFactory, id: string, label: string, onClick: Eve
         }, label))
 }
 
-function benchButtons(nodes: NodeFactory): Node {
+type BenchButtonsProps = {
+    onBuild: (count: number) => void
+};
+
+function benchButtons(nodes: NodeFactory, {onBuild}: BenchButtonsProps): Node {
     return nodes.el("div", {class: "col-md-6"},
         nodes.el("div", {class: "row"},
-           benchButton(nodes, "run", "Create 1,000 rows", (_) => {}),
-           benchButton(nodes, "runlots", "Create 10,000 rows", (_) => {}),
+           benchButton(nodes, "run", "Create 1,000 rows", (_) => onBuild(1000)),
+           benchButton(nodes, "runlots", "Create 10,000 rows", (_) => onBuild(10000)),
            benchButton(nodes, "add", "Append 1,000 rows", (_) => {}),
            benchButton(nodes, "update", "Update every 10th row", (_) => {}),
            benchButton(nodes, "clear", "Clear", (_) => {}),
            benchButton(nodes, "swaprows", "Swap rows", (_) => {})));
 }
+ 
+type RowProps = {
+    datumS: Signal<Datum>,
+    selectedS: Signal<number | undefined>
+    onClick: (id: number) => void,
+    onDelete: (id: number) => void
+};
+ 
+function row(nodes: NodeFactory, {datumS, selectedS, onClick, onDelete}: RowProps): Node {
+    const idS = datumS.map<number>(eq, (datum) => datum.id);
+    const isSelectedS = datumS.map2<boolean, number | undefined>(
+        eq,
+        (datum, selected) => datum.id === selected,
+        selectedS
+    );
 
-function createUI(nodes: NodeFactory): Node {
+    return nodes.el("tr",
+        {class: isSelectedS.map(eq, (isSelected) => isSelected ? "danger" : undefined)},
+        nodes.el("td", {class: "col-md-1"}, idS.map(eq, (id) => id.toString())),
+        nodes.el("td", {class: "col-md-4"},
+            nodes.el("a", {onclick: (_) => onClick(idS.ref())},
+                datumS.map(eq, (datum) => datum.label))),
+        nodes.el("td", {class: "col-md-1"},
+            nodes.el("a", {onclick: (_) => onDelete(idS.ref())},
+                nodes.el("span", {
+                    class: "glyphicon glyphicon-remove",
+                    'aria-hidden': "true"
+                }))),
+        nodes.el("td", {class: "col-md-6"}));
+}
+
+type TableProps = {
+    dataS: Vecnal<Datum>,
+    selectedS: Signal<number | undefined>,
+    onRowClick: (id: number) => void,
+    onRowDelete: (id: number) => void
+};
+
+function table(nodes: NodeFactory, {dataS, selectedS, onRowClick, onRowDelete}: TableProps): Node {
+    return nodes.el("table", {class: "table table-hover table-striped test-data"},
+        nodes.el("tbody", {},
+            nodes.forVecnal(dataS, (datumS) =>
+                row(nodes, {
+                    datumS, selectedS,
+                    onClick: onRowClick, onDelete: onRowDelete
+                }))));
+}
+
+function createUI(
+    nodes: NodeFactory,
+    modelS: Signal<Model>, selectedS: Signal<number | undefined>,
+    ctrl: Ctrl
+): Node {
     return nodes.el("div", {class: "container"},
         nodes.el("div", {class: "jumbotron"},
             nodes.el("div", {class: "row"},
                 heading(nodes),
-                benchButtons(nodes))));
+                benchButtons(nodes, {
+                    onBuild: (count) => ctrl.rebuild(count)
+                }))),
+                
+        table(nodes, {
+            dataS: vec.imux(eq, modelS.map(eq, (model) => new ImmArrayAdapter(model.data))),
+            selectedS,
+            onRowClick: (id) => ctrl.selectRow(id),
+            onRowDelete: id => ctrl.deleteRow(id)
+        }),
+        
+        nodes.el("span", {
+            class: "preloadicon glyphicon glyphicon-remove",
+            'aria-hidden': "true"}));
 }
 
 (() => {
     const nodeManager = new dom.NodeManager();
-    const ui = createUI(nodeManager);
+    
+    const modelS = sig.source(eq, new Model());
+    const selectedS = sig.source<number | undefined>(eq, undefined);
+    
+    const ctrl = new Ctrl(nodeManager, modelS, selectedS);
+    
+    const ui = createUI(nodeManager, modelS, selectedS, ctrl);
     const body = document.body;
     dom.insertBefore(body, ui, body.children[0]);
 })();
